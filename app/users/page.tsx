3ab02e8974e74a69f6e0a14ccc510377fdfd5c65
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-06-14 18:19:31 +0200

    init commit

---
+++ app/users/page.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import type { Metadata } from "next"
+
+export const metaData: Metadata = {
+    title: 'Users'
+}
+
+import getAllUsers from '@/lib/getAllUsers'
+import Link from 'next/link'
+
+
+export default async function UsersPage() {
+    const userData: Promise<User[]> = getAllUsers()
+    const users = await userData
+
+    const content = (
+        <section>
+            <h2>
+                <Link href='/'>Back to home</Link>
+            </h2>
+            <br />  
+            {users.map((user) => {
+                return (
+                    <>
+                    <p key={user.id}>
+                        <Link href={`/users/${user.id}`} >{user.name} </Link>
+                    </p>
+                    <br />
+                    </>
+                )
+            })}
+        </section>
+    )
+
+  return content
+}
